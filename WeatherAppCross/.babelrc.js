0d2e386b6c1cacf9fdcf729eded5d7366ad56c70
gebeto <slavik.nychkalo@gmail.com>
2018-11-23 09:52:35 +0200

    update

---
+++ WeatherAppCross/.babelrc.js
@@ -0,0 +1,22 @@
+
+module.exports = function(api) {
+	// console.log("!!!!HELLOWORLD!!!!!\n\n\n!!!!!!!!!!!!HELLO");
+
+	// const presets = ["module:metro-react-native-babel-preset"]
+	const presets = [
+		"react-native"
+	];
+
+	const plugins = [
+		["module-resolver", {
+			"alias": {
+				"react-native": "react-native-macos",
+			}
+		}]
+	];
+
+	return {
+		presets: presets,
+		plugins: plugins,
+	};
+};

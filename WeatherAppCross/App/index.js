e113528d1365047fb73b5389507d822528844a6a
gebeto <slavik.nychkalo@gmail.com>
2018-12-20 22:56:49 +0200

    upd

--- WeatherAppCross/App/index.js
+++ WeatherAppCross/App/index.js
@@ -10,10 +10,18 @@
 // https://www.wunderground.com/weather/api/?MR=1
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image
+} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  macos: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
@@ -21,12 +29,20 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  onImageSelectClick() {
+    alert("Hello");
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to Weather App Slavik!</Text>
+        <Text style={styles.welcome}>Welcome to Weather App Slavik Hello!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <TouchableOpacity style={styles.action} onPress={this.onImageSelectClick}>
+          <Text>Hello!</Text>
+        </TouchableOpacity>
+        <Image />
       </View>
     );
   }
@@ -49,5 +65,10 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  action: {
+    backgroundColor: 'black',
+    // color: '#fffffff',
+    padding: 5,
   },
 });
